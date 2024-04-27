6dd15e4966240a766dafa25f4705ca1d35f8dc6a
Urvi Shah <urvi.22111070@viit.ac.in>
2024-04-27 10:24:06 +0530

    final

--- README.md
+++
@@ -1,2 +0,0 @@
-Ideas : https://docs.google.com/spreadsheets/d/19mOQiNYAAlqdYsjdUYOTqZUzXdE00U3FrAHGyONNw60/edit?usp=sharing
-Also push repos with codes related to ideas
---
+++ exercises/__init__.py
@@ -0,0 +1,6 @@
+from .left_heel_slides import do_left_heel_slides, TOTAL_STEPS as LHS_TOTAL
+from .seated_right_knee_extension import do_seated_right_knee_extension, TOTAL_STEPS as SRKE_TOTAL
+from .side_lying_left_leg_lift import do_side_lying_left_leg_lift, TOTAL_STEPS as SLLLL_TOTAL
+
+
+# We're importing functions for specific leg exercises from separate files, along with constants indicating the total steps for each exercise. This allows us to use these functions and track the total steps easily.
